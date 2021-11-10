d0f77897b8bea0391212f3cc1a523e37baa9daca
Mary-Ann Nitzschke <m.a.raessler@gmail.com>
2021-11-10 14:23:45 +0100

    footer

---
+++ index.js
@@ -0,0 +1,10 @@
+const button = document.querySelector("#add")
+const input = document.querySelector(".suchfeld")
+const Papierkorb = document.querySelector("#trash")
+const hacken = document.querySelector("#check")
+
+function myButton(){
+    alert("hello world");
+}
+//button.addEventListener("click", myButton );
+console.log(button)
